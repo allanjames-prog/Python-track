3f760a73d6b52255d551b1b1c2832690ab29595f
Khiisa Allan James <james21.khiisa@gmail.com>
2025-03-06 15:41:08 +0300

    Added the content that explains OOP

--- tryIt.py
+++ tryIt.py
@@ -1,21 +1,4 @@
-# # Slicing strings
-# b = "Hello, World!"
-# print(b[2:5])
-
-# # Change to upper case
-# a = "Hello, World!"
-# print(a.upper())
-
-class Car:
-  def __init__(self, brand, model):
-    self.brand = brand  # Attribute
-    self.model = model  # Attribute
-  
-  def display_info(self):  # Method
-    return f"{self.brand} {self.model}"
-
-# Creating an object (instance of the Car class)
-my_car = Car("Toyota", "Corolla")
-
-# Output: Toyota Corolla
-print(my_car.display_info())  
+# Multiple Lines at Once
+lines = ["First line.\n", "Second line.\n", "Third line.\n"]
+with open("example.txt", "w") as file:  # Using 'with' automatically closes the file
+  file.writelines(lines)
